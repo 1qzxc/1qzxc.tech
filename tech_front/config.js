d46bae1c4f81519e42ed595bf940d3c4f81f7a84
1qzxc <cybermishamisha@gmail.com>
2024-08-26 17:28:16 -0700

    add nginx

--- tech_front/config.js
+++ tech_front/config.js
@@ -1,6 +1,6 @@
 // 👇️ named export
 //hostname = '192.168.1.9:1338';
-hostname = 'http://localhost:1338';
+hostname = 'https://1qzxc.tech';
 module.exports = hostname
-fronthostname = 'http://localhost:8084';
+fronthostname = 'https://1qzxc.tech';
 module.exports = fronthostname
